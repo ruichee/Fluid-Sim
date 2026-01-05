62fdaa486265b49753c73d96157c6b9d1b5ab95e
ruichee <maoruijie@yahoo.com>
2026-01-05 21:26:05 +0800

    Initial commit

---
+++ LidDrivenCavity/v2_stmfunc_vort.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from linear_solvers import Iterative as iter
+
+#########################################################################################################
+
+## Lid-Driven Cavity Flow (streamfunction-vorticity formulation) ##
+
+## poisson equation will be solved via Jacobi (Au=b) rather than stencil ##
+
+#########################################################################################################
+
+def poisson_assemble():
+    pass
+
+
+def poisson_solve():
+    pass
+
+
+def main():
+    pass
+
+
+def get_uv():
+    pass 
+
+
+def plot():
+    pass
+
